ab1873aa05fca5eb97da5cc172aa9ce745451eb5
M.abdallah <mohamad.abdalllah@gmail.com>
2020-12-22 21:59:10 +0200

    init service and controller

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +12,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::group(
+    ['namespace' => 'Api'],
+    function ($route) {
+        Route::get('listPopularRepos', 'SearchController@search');
+    }
+);
